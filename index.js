3c2747f5dd889f07556f82621209e5524a1a06b7
angelaaaaliu <liu.angela.1@gmail.com>
2023-02-20 12:49:35 -0800

    first push

--- index.js
+++ index.js
@@ -1,52 +1,261 @@
-import {
-    BoxGeometry,
-    Mesh,
-    MeshPhongMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-    DirectionalLight,
-  } from "three";
+let player;
+let gunEquipped = false;
+let scytheEquipped = false;
+let playerVel = 2;
+let lasers;
+let laserDirection;
+let laserVel = 7;
+let octomonHealthBar, playerHealthBar, octomonHealthBarOutline;
+let origHealthBarWidth;
+let slimes;
+let t = 0;
+
+function preload() {
+  bgImg = loadImage('imgs/bg.png');
+  playerFrontImg = loadImage('imgs/player-front.png');
+  // octomonSpriteSheet = loadSpriteSheet('imgs/octomon-hit.png', 'imgs/octomon.png')
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  player = createSprite (width / 4, height / 2);
+
+  player.width = width;
+  player.height = height;
+  player.scale = 0.00006316 * width;
+  player.addImg('front', 'imgs/player-front.png');
+  player.addImg('right', 'imgs/player-right.png');
+  player.addImg('left', 'imgs/player-left.png');
+  player.addImg('back', 'imgs/player-back.png')
+  player.addImg('rightGun', 'imgs/player-gun-right.png');
+  player.addImg('leftGun', 'imgs/player-gun-left.png');
+  player.addImg('frontGun', 'imgs/player-gun-front.png');
+  player.addImg('backGun', 'imgs/player-gun-back.png');
+  player.addImg('rightScythe', 'imgs/player-scythe-right.png');
+  player.addImg('leftScythe', 'imgs/player-scythe-left.png');
+  player.addImg('frontScythe', 'imgs/player-scythe-front.png');
+  player.addImg('backScythe', 'imgs/player-scythe-back.png');
+
+  player.ani = 'front';
+
+  octomon = createSprite(width - width / 4, height - height / 2.2, 'static');
+  octomon.width = width;
+  octomon.height = height * 2;
+  octomon.scale = 0.00007092 * width;
+  octomon.addImg('front', 'imgs/octomon.png');
+  octomon.spriteSheet = loadImage('imgs/octomon-hit.png');
+  // let octomonHitAni = loadAni('imgs/octomon-hit.png', 5);
+  // octomon.addAni('hit', 'imgs/octomon-hit.png', 5);
+  octomon.ani = 'front';
+
+  octomonHitBox = createSprite(width - width / 4, height / 2, width / 10, height / 10);
+  octomonHitBox.visible = false;
   
-  // Create our scene
-  const scene = new Scene();
-  
-  // Create the camera so we can see our scene
-  const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  
-  // Create our renderer and add it to the DOM
-  const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-  
-  // Create our cube mesh from  a geometry and a material and add it to the scene
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshPhongMaterial({ color: 0x00ff00 });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
-  
-  // Add a directional light so we can see shadows on the cube
-  const color = 0xffffff;
-  const intensity = 1;
-  const light = new DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  scene.add(light);
-  
-  // Position the camera
-  camera.position.z = 5;
-  
-  // The animation loop updates the cube's rotation
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+  octomonHealthBar = createSprite(windowWidth - windowWidth / 5, 100, width / 5, height / 80, 'static');
+  octomonHealthBar.color = color(255, 0, 0);
+  octomonHealthBar.strokeWeight = 0;
+  origHealthBarWidth = octomonHealthBar.width;
+  // console.log(origHealthBarWidth);
+  octomonHealthBarOutline = createSprite(width - width / 5, 100, width / 5, height / 80, 'staticsaw');
+  octomonHealthBarOutline.color = color(0, 0, 0, 0);
+  octomonHealthBarOutline.stroke = color(255, 246, 192);
+
+  lasers = new Group();
+
+  slimes = new Group();
+
+  player.overlaps(octomon);
+  player.overlaps(lasers);
+  lasers.collides(octomon, octomonLaserHit);
+  octomon.overlaps(octomonHitBox);
+  player.overlaps(octomonHitBox);
+  octomonHitBox.overlaps(lasers);
+  octomonHealthBar.overlaps(player);
+  octomonHealthBar.overlaps(lasers);
+  octomonHealthBarOutline.overlaps(player);
+  octomonHealthBarOutline.overlaps(lasers);
+  slimes.overlaps(octomon);
+  slimes.overlaps(octomonHealthBar);
+  slimes.overlaps(octomonHealthBarOutline);
+  slimes.overlaps(lasers);
+
+  player.layer = 3;
+  lasers.layer = 2;
+  slimes.layer = 2;
+  octomon.layer = 1;
+}
+
+function draw() {
+  background(1, 46, 55);
+  allSprites.debug = mouse.pressing();
+  image(bgImg, 0, 0, width, height, 0, 0, bgImg.width, bgImg.height, CONTAIN);
+  fill(255, 246, 192);
+  t++;
+  if (t % 150 == 0) {
+    throwSlime();
   }
+  // rect(width / 4, height / 2, width / 15, width / 20);
+  // rect(width - width / 6, 100, width / 6, height / 80);
+  // rect(width - width / 4, height - height / 2.5, width / 10, height / 5);
+  // if (keyCode === 69 && weaponEquipped == false) {
+  //   weaponEquipped = true;
+  //   console.log(weaponEquipped);
+  // }
   
-  // Start the animation loop
-  animate();
+  // if (keyCode === 69 && weaponEquipped == true) {
+  //   weaponEquipped = false;
+  //   console.log(weaponEquipped);
+  // }
+
+
+  if(kb.presses('1') && gunEquipped == false) {
+    gunEquipped = true;
+    scytheEquipped = false;
+  } else if (kb.presses('1') && gunEquipped == true) {
+    gunEquipped = false;
+  }
+
+  if(kb.presses('2') && scytheEquipped == false) {
+    scytheEquipped = true;
+    gunEquipped = false;
+  } else if (kb.presses('2') && scytheEquipped == true) {
+    scytheEquipped = false;
+  }
+
+  // console.log(scytheEquipped);
+
+  // console.log(gunEquipped);
+
+  if (gunEquipped == false && scytheEquipped == false) {
+    if(kb.pressing('right')) {
+      player.ani = 'right';
+      player.vel.x = 4;
+    } else if (kb.pressing('left')) {
+      player.ani = 'left';
+      player.vel.x = -4;
+    } else if (kb.pressing('down')) {
+      player.ani = 'front';
+      player.vel.y = 4;
+    } else if (kb.pressing('up')) {
+      player.ani = 'back';
+      player.vel.y = -4;
+    } else {
+      player.vel.x = 0;
+      player.vel.y = 0;
+    }
+  } else if (gunEquipped) {
+    if(kb.pressing('right')) {
+      player.ani = 'rightGun';
+      laserDirection = 'right';
+      player.vel.x = playerVel;
+    } else if (kb.pressing('left')) {
+      player.ani = 'leftGun';
+      laserDirection = 'left';
+      player.vel.x = -playerVel;
+    } else if (kb.pressing('down')) {
+      player.ani = 'frontGun';
+      laserDirection = 'down';
+      player.vel.y = playerVel;
+    } else if (kb.pressing('up')) {
+      player.ani = 'backGun';
+      laserDirection = 'up';
+      player.vel.y = -playerVel;
+    } else {
+      player.vel.x = 0;
+      player.vel.y = 0;
+    }
+  } else if (scytheEquipped) {
+    if(kb.pressing('right')) {
+      player.ani = 'rightScythe';
+      player.vel.x = playerVel;
+    } else if (kb.pressing('left')) {
+      player.ani = 'leftScythe';
+      player.vel.x = -playerVel;
+    } else if (kb.pressing('down')) {
+      player.ani = 'frontScythe';
+      player.vel.y = playerVel;
+    } else if (kb.pressing('up')) {
+      player.ani = 'backScythe';
+      player.vel.y = -playerVel;
+    } else {
+      player.vel.x = 0;
+      player.vel.y = 0;
+    }
+  }
+
+  if (gunEquipped) {
+    shootLaser();
+  }
+
+  octomonScytheHit();
+
+  if (octomonHealthBar.width < 1) {
+    win();
+  }
+
+}
+
+function shootLaser() {
+  if (kb.presses('k')) {
+    let laser = new lasers.Sprite(player.x, player.y);
+    laser.color = color(255, 34, 239);
+    laser.stroke = color(255, 246, 192);
+    laser.diameter = width / 220;
+    if (laserDirection == 'right') {
+      laser.vel.x = laserVel;
+      laser.vel.y = 0;
+    } else if (laserDirection == 'left') {
+      laser.vel.x = -laserVel;
+      laser.vel.y = 0;
+    } else if (laserDirection == 'up') {
+      laser.vel.x = 0;
+      laser.vel.y = -laserVel;
+    } else {
+      laser.vel.x = 0;
+      laser.vel.y = laserVel;
+    }
+    laser.life = 100;
+  }
+}
+
+function octomonLaserHit() {
+  // octomon.visible = false;
+  octomonHealthBar.width = octomonHealthBar.width - origHealthBarWidth / 100;
+  octomonHealthBar.x = octomonHealthBar.x - origHealthBarWidth / 200;
+  // octomon.ani = 'hit';
+  console.log(octomonHealthBar.width);
+}
+
+function octomonScytheHit() {
+  if(scytheEquipped) {
+    if (kb.presses('k') && player.overlapping(octomon)) {
+      octomonHealthBar.width = octomonHealthBar.width - origHealthBarWidth / 10;
+      octomonHealthBar.x = octomonHealthBar.x - origHealthBarWidth / 20;
+    }
+  }
+}
+
+function throwSlime() {
+    let slime = new slimes.Sprite(octomon.x, octomon.y);
+    slime.scale = 0.00006382979 * width;
+    slime.spriteSheet = loadImage('imgs/slime-sprite-sheet.png', { frameSize: [900, 600], frames: 6});
+    slime.addImg('thrown', [
+      [0, 0],
+      [300, 0],
+      [600, 0],
+      [0, 300],
+      [300, 300],
+      [600, 300]
+    ]);
+    slime.ani.frameDelay = 16;
+    slime.ani.looping = false;
+    slime.diameter = width / 20;
+    console.log(t);
+    slime.life = 1500;
+}
+
+function win() {
+  octomon.collider = 'kinematic';
+  octomon.rotationSpeed = 1;
+  octomon.vel.x = 4;
+}
